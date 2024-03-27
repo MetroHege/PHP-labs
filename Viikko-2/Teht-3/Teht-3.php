30cdd9969c1827c5bb489f63bfb9750dc6af07d1
MetroHege <113589548+MetroHege@users.noreply.github.com>
2024-03-27 15:38:30 +0200

    Teht-3

--- Viikko-2/Teht-3/Teht-3.php
+++ Viikko-2/Teht-3/Teht-3.php
@@ -1,1 +1,29 @@
 <?php
+session_start();
+$username = '';
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['remember'])) {
+        $username = $_POST['username'];
+        $_SESSION['username'] = $username;
+    } else {
+        if(isset($_SESSION['username'])) {
+            unset($_SESSION['username']);
+        }
+        $username = '';
+    }
+} else {
+    if(isset($_SESSION['username'])) {
+        $username = $_SESSION['username'];
+    }
+}
+?>
+
+<form method="post">
+    Username:
+    <input type="text" name="username" value="<?php echo $username; ?>" required>
+    <br>
+    Remember me:
+    <input type="checkbox" name="remember" <?php echo isset($_SESSION['username']) ? 'checked' : ''; ?>>
+    <br>
+    <input type="submit" value="Submit">
+</form>
