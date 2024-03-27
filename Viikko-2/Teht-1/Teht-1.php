618a1b6c2d19a3e9d6e3522e9b1dc9e21aef3a82
MetroHege <113589548+MetroHege@users.noreply.github.com>
2024-03-27 14:38:24 +0200

    Teht-1

--- Viikko-2/Teht-1/Teht-1.php
+++ Viikko-2/Teht-1/Teht-1.php
@@ -1,1 +1,42 @@
 <?php
+$style = "";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $color = $_POST['color'];
+    $size = $_POST['size'];
+    $fontStyle = isset($_POST['fontStyle']) ? $_POST['fontStyle'] : [];
+
+    $style = "color: $color; font-size: $size;";
+    if (in_array('bold', $fontStyle)) {
+        $style .= " font-weight: bold;";
+    }
+    if (in_array('italic', $fontStyle)) {
+        $style .= " font-style: italic;";
+    }
+}
+?>
+
+    <form method="post">
+        Color:
+        <input type="radio" name="color" value="red" required> Red
+        <input type="radio" name="color" value="green"> Green
+        <input type="radio" name="color" value="blue"> Blue
+        <br>
+        Size:
+        <select name="size" required>
+            <option value="small">Small</option>
+            <option value="medium">Medium</option>
+            <option value="large">Large</option>
+        </select>
+        <br>
+        Font style:
+        <input type="checkbox" name="fontStyle[]" value="bold"> Bold
+        <input type="checkbox" name="fontStyle[]" value="italic"> Italic
+        <br>
+        <input type="submit" value="Submit">
+    </form>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    echo "<p style=\"$style\">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>";
+}
+?>
